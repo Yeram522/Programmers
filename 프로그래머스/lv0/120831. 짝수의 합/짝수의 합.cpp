d312e2217728d50f92587454e2498c2a20add248
Marey <63442636+Yeram522@users.noreply.github.com>
2023-01-26 18:28:49 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    for(int i = 1; i <= n ; i++)
+    {
+        answer += i%2 == 0 ? i :0;
+    }
+    
+    return answer;
+}
