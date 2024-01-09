73b5397b2f546ca9365f0cb68d2383ac3154748d
Marey <63442636+Yeram522@users.noreply.github.com>
2024-01-09 21:50:14 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -8,71 +8,40 @@ const int n_max = 70;
 
 int dy[4] = { -1, 0, 1, 0 };
 int dx[4] = { 0,  1, 0 , -1};
-int N,sx,sy, ex,ey,a[n_max][n_max], visited[n_max][n_max];
-
-//해당 범위 내의 1의 개수를 보고, 판단.
-int dfs(int y, int x)
-{
-    visited[y][x] = 1; //방문처리
-    
-    int count = 0;
-    for(int i = 0; i < 4; i++)
-    {
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-        
-        if(ny < sy || ny > ey || nx < sx || nx > ex || visited[ny][nx] == 1) continue;
-        
-        count += a[ny][nx];
-        
-        count += dfs(ny, nx);
-    }
-    
-    return count;
-}
+int N,a[n_max][n_max];
 
 //quad tree라는 재귀 함수를 정의함
 string quadtree(int n, int y, int x)
 {
-    //4분할로 나눠서 탐색
-    string ret = "";
-    // 범위 초기화화
-    sy = y;//2
-    sx = x;
-    
-    ey = y+n-1;
-    ex = x+n-1;
-   
-    int starty = sy;
-    int startx = sx;
-    int endy = ey;
-    int endx = ex;
-    
     if( n == 1)
     {
         return to_string(a[y][x]);
     }
-    //visited 초기화
-    fill(&visited[0][0], &visited[0][0] + n_max*n_max, 0);
     
-    int res = dfs(sy,sx) + (a[sy][sx] == 1 ? 1: 0);
+    int b = a[y][x];
+    
+    string ret = "";
 
-    if(res == n*n || res == 0) //구역전체가 1또는 0이라면 string에 1또는 0 추가가
+    for(int i = y; i < y + n ; i++)
     {
-        ret += (res == 0 ? "0" : "1");
+        for(int j = x; j < x + n; j++)
+        {
+            if(b != a[i][j])
+            {
+                int half = n/2;
+                ret += "(";
+                ret += quadtree(half, y, x );
+                ret += quadtree(half, y, x + half );
+                ret += quadtree(half, y + half ,x);
+                ret += quadtree(half, y + half,x + half);
+                ret += ")";
+                
+                return ret;
+            }
+        }
     }
-    else //아니라면 재귀!
-    {
-        int half = n/2;
-        ret += "(";
-        ret += quadtree(n/2, starty, startx );
-        ret += quadtree(n/2, starty, startx + n/2 );
-        ret += quadtree(n/2, starty + n/2 ,startx);
-        ret += quadtree(n/2, starty + n/2,startx + n/2);
-        ret += ")";
-    }
-    
-    return ret;
+   
+    return to_string(a[y][x]);
     
 }
 
@@ -87,12 +56,7 @@ int main() {
             scanf("%1d", &a[i][j]);
         }
     }
-    
-    sy = 0;
-    sx = 0;
-    
-    ey = N-1;
-    ex = N-1;
+
     
     cout << quadtree(N, 0,0) ;
 
